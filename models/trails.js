3221d89a4baf8a359b6c07f90b4701dd27dc659a
jvan1789 <jas_vanexel89@aol.com>
2022-07-23 12:55:36 -0400

    jv started seeding

---
+++ models/trails.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const trailsSchema = new Schema({
+   name: {type: String, required: true},
+   location: {type: String, required: true},
+   difficulty: {type: Number, required: true},
+   length: {type: Number, required: true},
+   elevationChange: {type: Number, required: true},
+   routeType: {type: String, required: true},
+   description: {type: String, required: true},
+   image: {type: String, required: true},
+   cloudinary_id: {type: String, required: true},
+   reviews: mongoose.Schema.Types.ObjectId, ref: 'Review',
+})
+
+model.exports = mongoose.model('Trail', trailsSchema);
