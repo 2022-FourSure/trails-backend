aca47c8fdce418651e12c784d92eda0e7ad41458
Catalina <102118452+cachelina@users.noreply.github.com>
2022-07-23 15:19:57 -0700

    Revert "feat(Authentication): Scaffold Authentication routes"

--- helpers/haltOnTimedout.js
+++
@@ -1,9 +0,0 @@
-const haltOnTimedout = (req, res, next) => {
-  if (!req.timedout) {
-    next();
-  } else {
-    res.status(503).json({ error: "Error: Response timeout" });
-  }
-};
-
-module.exports = haltOnTimedout;
--- helpers/isPresent.js
+++
@@ -1,6 +0,0 @@
-const isNullOrUndefined = require("./isNullOrUndefined");
-const isEmpty = require("./isEmpty");
-
-const isPresent = (data) => !isEmpty(data) && !isNullOrUndefined(data);
-
-module.exports = isPresent;
