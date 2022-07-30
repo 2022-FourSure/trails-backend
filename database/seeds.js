c4f717361a130bf37bd00e2af05a421194890cae
carolinexc <caroline@humanperspectives.com>
2022-07-30 15:57:43 -0400

    finished backend for reviews

--- database/seeds.js
+++ database/seeds.js
@@ -24,7 +24,7 @@ Trail.deleteMany({})
             // for the position in the reviewSeeds array it should correspond to.
             // We find the review with text that matches the text of the review
             // in that position and push it to the trails array. 
-            trail.reviews.push(reviews.find(r => reviewSeeds[trail.reviewss[0]].text === r.text)._id)
+            trail.reviews.push(reviews.find(r => reviewSeeds[trail.reviewss[0]].content === r.content)._id)
             return trail
         })
         // Save the newly created trails to the database
