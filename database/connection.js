444c69e3022c096723092a8dff5ab85371f2eaf4
carolinexc <caroline@humanperspectives.com>
2022-08-02 23:11:08 -0400

    tested db, populated seed data locally

--- database/connection.js
+++ database/connection.js
@@ -2,35 +2,35 @@ const mongoose = require("mongoose");
 // Require dotenv library so can use variables in .env file
 require("dotenv").config();
 
-// const mongoURI = 'mongodb://localhost:27017/trails'
+const mongoURI = 'mongodb://localhost:27017/trails'
 
-// mongoose.connect(mongoURI, {
-//   useNewUrlParser: true
-// })
-// // Instantiate a variable for the mongoose connection
-// const db = mongoose.connection
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true
+})
+// Instantiate a variable for the mongoose connection
+const db = mongoose.connection
 
-// // Once the database is connected, console log the db host and port
-// db.on('connected', () => {
-//   console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
-// })`
+// Once the database is connected, console log the db host and port
+db.on('connected', () => {
+  console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
+})
 
 // CC: CAT'S CODE THAT I COMMENTED OUT TEMPORARILY
-const databaseUrl = `mongodb+srv://${process.env.TRAILS_DB_USERNAME}:${process.env.TRAILS_DB_PASSWORD}@${process.env.TRAILS_DB_URI}`;
-const dbOptions = {
-  useNewUrlParser: true,
-  dbName: "trailsdb",
-  useUnifiedTopology: true,
-};
+// const databaseUrl = `mongodb+srv://${process.env.TRAILS_DB_USERNAME}:${process.env.TRAILS_DB_PASSWORD}@${process.env.TRAILS_DB_URI}`;
+// const dbOptions = {
+//   useNewUrlParser: true,
+//   dbName: "trailsdb",
+//   useUnifiedTopology: true,
+// };
 
-mongoose.connect(databaseUrl, dbOptions).catch((err) => console.log(err));
+// mongoose.connect(databaseUrl, dbOptions).catch((err) => console.log(err));
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
-db.on("connected", () => console.log("connected to Mongo"));
+// db.on("connected", () => console.log("connected to Mongo"));
 
-db.once("open", function () {
-  db.collection("collection", function (err, collection) {
-    collection.find({}).toArray(function (err, data) {});
-  });
-});
+// db.once("open", function () {
+//   db.collection("collection", function (err, collection) {
+//     collection.find({}).toArray(function (err, data) {});
+//   });
+// });
